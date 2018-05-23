ee2d82f39598631666f9af73b4fd422a8b164bed
Denise Pendleton <dpendl1@hotmail.com>
2018-05-23 23:07:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,16 +5,18 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
-    
+
   end
 
   def new
     @song = Song.new
-    
+
   end
 
   def create
     @song = Song.new(song_params)
+    @artist = Artist.find(@song.artist_id)
+    @artist.songs << @song
 
     if @song.save
       redirect_to @song
