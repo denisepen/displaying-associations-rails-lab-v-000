bb096027401e3c067f16e27b25f39a84f82b15aa
Denise Pendleton <dpendl1@hotmail.com>
2018-05-23 23:47:30 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,10 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
-
-    @song = Song.find(params[:id])
-    @song.artist.name - @song.title
-  end
+  # def artist_name
+  #
+  #   @song = Song.find(params[:id])
+  #   @song.artist.name - @song.title
+  # end
 
 end
